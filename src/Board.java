56805f829ea84bacc781cdb87c2380c4356444d9
Ivan Timofeev <alexrush1@yandex.ru>
2021-02-28 01:59:20 +0700

    about

--- src/Board.java
+++ src/Board.java
@@ -1,8 +1,11 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Board {
 
@@ -23,6 +26,7 @@ public class Board {
         JMenuBar menuBar = new JMenuBar();
         menuBar.add(createFileMenu());
         menuBar.add(createViewMenu());
+        menuBar.add(createAboutMenu());
         frame.setJMenuBar(menuBar);
 
         JToolBar toolsBar = new JToolBar();
@@ -129,12 +133,48 @@ public class Board {
     {
         // создадим выпадающее меню
         JMenu viewMenu = new JMenu("Tools");
-        JMenuItem resize = new JMenuItem("Resize");
 
+        JMenuItem clearButton = new JMenuItem("Clear");
+        clearButton.addActionListener(this::clear);
+
+        JMenuItem colorChooser = new JMenuItem("Color Chooser");
+        colorChooser.addActionListener(this::colorChooser);
+
+        JMenuItem resize = new JMenuItem("Resize");
         resize.addActionListener(this::resize);
 
-        viewMenu.addSeparator();
+        JRadioButton cursorButton = new JRadioButton("Cursor");
+        cursorButton.addActionListener(this::cursor);
+
+        JRadioButton linesButton = new JRadioButton("Lines");
+        linesButton.addActionListener(this::lines);
+
+        JRadioButton stampButton = new JRadioButton("Stamp");
+        stampButton.addActionListener(this::stamp);
+
+        JRadioButton starStampButton = new JRadioButton("Star Stamp");
+        starStampButton.addActionListener(this::starStamp);
+
+        JRadioButton fillButton = new JRadioButton("Fill");
+        fillButton.addActionListener(this::fill);
+
+        ButtonGroup buttonGroup = new ButtonGroup();
+
+        buttonGroup.add(cursorButton);
+        buttonGroup.add(linesButton);
+        buttonGroup.add(stampButton);
+        buttonGroup.add(starStampButton);
+        buttonGroup.add(fillButton);
+
+        viewMenu.add(clearButton);
+        viewMenu.add(colorChooser);
         viewMenu.add(resize);
+        viewMenu.addSeparator();
+        viewMenu.add(cursorButton);
+        viewMenu.add(linesButton);
+        viewMenu.add(stampButton);
+        viewMenu.add(starStampButton);
+        viewMenu.add(fillButton);
         // меню-флажки
 //        JCheckBoxMenuItem line  = new JCheckBoxMenuItem("Линейка");
 //        JCheckBoxMenuItem grid  = new JCheckBoxMenuItem("Сетка");
@@ -157,10 +197,39 @@ public class Board {
         return viewMenu;
     }
 
+    private JMenu createAboutMenu() {
+        JMenu viewMenu = new JMenu("About");
+        JMenuItem about = new JMenuItem("About");
+        about.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame aboutFrame = new JFrame("About");
+                aboutFrame.setSize(250, 120);
+                aboutFrame.setLayout(null);
+                JLabel icon = new JLabel();
+                icon.setIcon(new ImageIcon("./src/resources/author.JPG"));
+                icon.setBounds(7,7,70,70);
+                JLabel infoName = new JLabel("Timofeev Ivan");
+                infoName.setBounds(115, 15, 140, 20);
+                JLabel groupName = new JLabel("18204");
+                groupName.setBounds(135, 35, 50, 20);
+                JLabel email = new JLabel("i.timofeev2@g.nsu.ru");
+                email.setBounds(95, 55, 140, 20);
+                aboutFrame.add(icon);
+                aboutFrame.add(infoName);
+                aboutFrame.add(groupName);
+                aboutFrame.add(email);
+                aboutFrame.setVisible(true);
+            }
+        });
+        viewMenu.add(about);
+        return viewMenu;
+    }
+
     class ExitAction extends AbstractAction {
         private static final long serialVersionUID = 1L;
         ExitAction() {
-            putValue(NAME, "Выход");
+            putValue(NAME, "Exit");
         }
         public void actionPerformed(ActionEvent e) {
             System.exit(0);
